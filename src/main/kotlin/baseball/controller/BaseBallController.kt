c7d25c6aa07e4ed52038ffd9439850b351ef0063
HamBeomJoon <hbj0802@naver.com>
2024-09-28 00:54:52 +0900

    feat: 서로 다른 3자리 수인지 유효성 체크

--- src/main/kotlin/baseball/controller/BaseBallController.kt
+++ src/main/kotlin/baseball/controller/BaseBallController.kt
@@ -3,6 +3,8 @@ package baseball.controller
 import baseball.model.MagicNumberGenerator
 import baseball.view.InputView
 import baseball.view.OutputView
+import camp.nextstep.edu.missionutils.Console
+import org.junit.jupiter.api.assertThrows
 
 class BaseBallController {
     private val inputView = InputView()
@@ -16,6 +18,12 @@ class BaseBallController {
 
         inputView.printInputUserNum()
         val userInput = Console.readLine()
+        if (userInput.isEmpty() || userInput.length > 3) {
+            throw IllegalArgumentException("3자리 수를 입력해야 합니다.")
+        }
+        if (userInput[0] == userInput[1] || userInput[0] == userInput[2] || userInput[1] == userInput[2]) {
+            throw IllegalArgumentException("서로 다른 3자리 수를 입력해야 합니다.")
+        }
     }
 
 }
