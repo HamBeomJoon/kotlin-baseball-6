3510db8739ae545045fe0afa685f89f01670d01a
HamBeomJoon <hbj0802@naver.com>
2024-09-28 00:55:08 +0900

    feat: 사용자 입력 숫자 1~9 사이인지 유효성 체크

--- src/main/kotlin/baseball/controller/BaseBallController.kt
+++ src/main/kotlin/baseball/controller/BaseBallController.kt
@@ -24,6 +24,11 @@ class BaseBallController {
         if (userInput[0] == userInput[1] || userInput[0] == userInput[2] || userInput[1] == userInput[2]) {
             throw IllegalArgumentException("서로 다른 3자리 수를 입력해야 합니다.")
         }
+        for (num in userInput) {
+            if (num < '1' || num > '9') {
+                throw IllegalArgumentException("1 ~ 9 사이의 숫자만 입력해야 합니다.")
+            }
+        }
     }
 
 }
