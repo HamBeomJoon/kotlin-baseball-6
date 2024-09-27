90a9ed5926d1bb412ceb36da6778d804292854fa
HamBeomJoon <hbj0802@naver.com>
2024-09-27 20:26:56 +0900

    feat: 숫자 야구 게임 시작 출력

---
+++ src/main/kotlin/baseball/controller/BaseBallController.kt
@@ -0,0 +1,1 @@
+package baseball.controllerimport baseball.view.InputViewimport baseball.view.OutputViewclass BaseBallController {    private val inputView = InputView()    private val outputView = OutputView()    fun playGame() {        inputView.printPlayGame()    }}
