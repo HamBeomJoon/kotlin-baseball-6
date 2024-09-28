48150ec414ac77550c11572545cacc813f60e4e2
HamBeomJoon <hbj0802@naver.com>
2024-09-28 14:27:54 +0900

    fix: 예외처리 조건 수정

--- src/main/kotlin/baseball/controller/BaseBallController.kt
+++ src/main/kotlin/baseball/controller/BaseBallController.kt
@@ -42,13 +42,13 @@ class BaseBallController {
             if (strike == 3) {
                 gameOver()
 
-            userInput = Console.readLine()
-            if (userInput.isEmpty() || userInput.length > 1) {
-                throw IllegalArgumentException("1과 2중 하나만 입력해야 합니다.")
-            }
-            if (userInput[0] != '1' || userInput[0] != '2') {
-                throw IllegalArgumentException("1과 2중 하나만 입력해야 합니다.")
-            }
+                userInput = Console.readLine()
+                if (userInput.isEmpty() || userInput.length > 1) {
+                    throw IllegalArgumentException("1과 2중 하나만 입력해야 합니다.")
+                }
+                if (userInput[0] != '1' && userInput[0] != '2') {
+                    throw IllegalArgumentException("1과 2중 하나만 입력해야 합니다.")
+                }
 
                 if (userInput[0] == '1') {
                     playGame()
